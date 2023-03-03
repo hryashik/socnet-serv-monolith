f053946289ff22b0a6517058af9930ad40b10b72
hryashik <tsukikohaya@gmail.com>
2023-03-03 16:55:51 +0300

    create and connect prisma module

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(private readonly config: ConfigService) {
+    console.log(config.get('DATABASE_URL'))
+    super({
+      datasources: {
+        db: {
+          url: config.get("DATABASE_URL"),
+        },
+      },
+    });
+  }
+}
