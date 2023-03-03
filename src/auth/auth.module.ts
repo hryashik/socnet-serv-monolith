d39913209882b2e761bcfc06183a97d509b8d3c4
hryashik <tsukikohaya@gmail.com>
2023-03-03 17:59:54 +0300

    realize sign token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import {config} from 'dotenv'
 
 @Module({
+  imports: [JwtModule.register({
+    signOptions: {
+      expiresIn: '24h'
+    },
+    secret: process.env.JWT_SECRET
+  })],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,38 +1,49 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SignUpDto } from './dto/signup.dto';
-import * as argon from 'argon2'
+import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   async signup(dto: SignUpDto) {
     try {
-      const hash = await this.hashPassword(dto.password)
+      const hash = await this.hashPassword(dto.password);
       const user = await this.prismaService.user.create({
         data: {
           email: dto.email,
-          hash
+          hash,
         },
         select: {
           email: true,
           createdAt: true,
           updatedAt: true,
-        }
-      })
-      return user
+        },
+      });
+      return this.signToken(user.email);
     } catch (error) {
       //P2002 - prisma error if unique filed is taken
-      if (error.code === 'P2002') throw new ForbiddenException('Credentials is taken')
-      throw new Error(error)
+      if (error.code === 'P2002')
+        throw new ForbiddenException('Credentials is taken');
+      throw new Error(error);
     }
   }
 
- hashPassword(password: string): Promise<string> {
+  hashPassword(password: string): Promise<string> {
     const hash = argon.hash(password, {
       saltLength: 8,
-    })
-    return hash
+    });
+    return hash;
+  }
+  signToken(email: string) {
+    const token = this.jwtService.sign({ email });
+    return {
+      access_token: token,
+    };
   }
 }
